2b1dfb8dde27871992c313fd72b1e604b6ab88f0
lottehof <s1112052@student.hsleiden.nl>
2021-03-18 15:34:11 +0100

    debugging

--- dist/_nuxt/static/1616077578/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","contact","\u002Foverons","\u002Fjachthaven","\u002F","\u002Fhand-made","nieuws\u002Faluminium-als-basis","\u002Fnieuws","\u002Fnieuws\u002Faluminium-als-basis"]})
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -17,7 +17,7 @@
       </div>
       <div class="container-content">
         <h2 class="contact-title">Contact ons</h2>
-        <form method="post" name="contact" data-netlify-recaptcha="true" data-netlify="true">
+        <form method="post" name="contact" >
           <div class="group">
             <input type="text" name="name" placeholder="Naam" >
           </div>
@@ -31,7 +31,7 @@
             <textarea rows="8" name="message" cols="80" placeholder="Bericht"></textarea>
           </div>
           <div class="group">
-            <!-- <recaptcha class="recaptcha" @error="onError" @success="onSuccess" @expired="onExpired" /> -->
+            <recaptcha class="recaptcha" @error="onError" @success="onSuccess" @expired="onExpired" />
           </div>
           <div class="group">
             <input class="form-button" type="submit" value="Versturen" />
