94dd8dabffcc4e66c5435b067c0c569deec5b8b1
lottehof <s1112052@student.hsleiden.nl>
2021-03-18 09:51:14 +0100

    testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ export default {
     recaptcha: {
     hideBadge: false, // Hide badge element (v3 & v2 via size=invisible)
     siteKey: '6LcnSYQaAAAAAEsIat4dz6PTuGCyYpQF2JQ3vdFs', // Site key for requests
-    version: 3, // Version
+    version: 2, // Version
     size: 'invisible' // Size: 'compact', 'normal', 'invisible' (v2)
   },
 
